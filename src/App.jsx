28c800384262111c7419de46b9b6b5684bca9d1c
AliHaidarAzizi <135135657+AliHaidarAzizi@users.noreply.github.com>
2023-08-15 00:32:57 +0800

    improvement form and h1 hello user

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import Calculator from './components/Calculator'
 import Counter from './components/Counter'
 import TodoComponent from './components/TodoComponent'
-// import Modal from './components/Modal'
+import Modal from './components/Modal'
+import Form from './components/Form'
 
 function App() {
+  const [name, setName] = useState('World')
 
+  const OnSubmitProps1 =(event) => {
+    event.preventDefault();
+    const newValue = event.target.userName.value
+
+    
+    setName(newValue)
+  }
+    
 
   return (
     <>
-    <div className='bg-gray-300 dark:bg-slate-800 min-h-screen flex flex-col items-center justify-center font-sans gap-3 p-4'> 
-      <h1 className=' text-white text-5xl mb-2'>Hello World!</h1>
-      {/* <Modal /> */}
+    <div className=' dark:bg-slate-800 min-h-screen flex flex-col items-center justify-center font-sans gap-3 p-4'> 
+      <h1 className=' text-white text-5xl mb-2'>Hello {name} 
+      </h1>
       <div className='flex gap-3 items-center justify-center flex-wrap'>
+          <Form 
+            onSubmitProps={OnSubmitProps1}
+            />
           <div className='flex flex-col gap-3 flex-wrap justify-center'>
             <TodoComponent />
             <Counter />
@@ -27,7 +40,8 @@ function App() {
     
   )
 
-
 }
+
+// }
 
 export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,26 @@
+// import { useState } from "react";
+
+function Form({onSubmitProps}) {
+
+
+    return(
+        <>
+        <div className="text-white">
+            <h2>Basic Information</h2>
+            <form  className="flex flex-col " onSubmit={onSubmitProps} action="">
+                <input type="text" name="userName" className="text-black"  placeholder="Fill in your Name" 
+                />
+                {/* <input type="number" name="userAge" className="text-black" placeholder="Fill in your Age" /> */}
+                <button type="submit">Send
+                </button>
+
+            </form>
+        </div>
+        
+    
+        </>
+        )
+        
+}
+
+export default Form;
