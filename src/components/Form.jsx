1bbba304de937feb7879c2abe825c558562b1277
AliHaidarAzizi <135135657+AliHaidarAzizi@users.noreply.github.com>
2023-08-15 21:19:56 +0800

    cosmetic design of send button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,10 +12,10 @@ function Form({onSubmitProps}) {
         <div className="text-white">
             <h2>Basic Information</h2>
             <form  className="flex flex-col " onSubmit={onSubmitProps} action="">
-                <input type="text" name="userName" className="text-black"  placeholder="Fill in your Name" 
+                <input type="text" name="userName" className="text-black rounded-sm"  placeholder="Fill in your Name" 
                 />
                 {/* <input type="number" name="userAge" className="text-black" placeholder="Fill in your Age" /> */}
-                <button type="submit">Send
+                <button type="submit" className="mt-2 bg-slate-300 text-slate-800 rounded-sm hover:bg-slate-900 hover:text-white ease-in-out duration-300">Send
                 </button>
 
             </form>
